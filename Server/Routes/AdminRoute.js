f5ce44703585bed1147c0b8355688e658fc7ebed
lebotreasure <lebohangsylviak@gmail.com>
2023-11-08 14:32:40 +0200

    Employee router

--- Server/Routes/AdminRoute.js
+++ Server/Routes/AdminRoute.js
@@ -1,6 +1,9 @@
 import express from "express";
 import con from "../utils/db.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import multer from "multer";
+import path from "path";
 
 const router = express.Router();
 
@@ -14,7 +17,7 @@ router.post('/adminlogin', (req, res) => {
                 { role: "admin", email: email },
                 "jwt_secret_key",
                 { expiresIn: "1d" }
-                
+
             );
             res.cookie('token', token);
             return res.json({ loginStatus: true });
@@ -27,16 +30,62 @@ router.post('/adminlogin', (req, res) => {
 router.get('/category', (req, res) => {
     const sql = "SELECT * FROM category";
     con.query(sql, (err, result) => {
-        if(err) return res.json({Status: false, Error: "Query error"})
-        return res.json({Status: true, Result: result})
+        if (err) return res.json({ Status: false, Error: "Query error" })
+        return res.json({ Status: true, Result: result })
     });
 });
 
 router.post('/add_category', (req, res) => {
     const sql = "INSERT INTO category (`name`) VALUES (?)";
     con.query(sql, [req.body.category], (err, result) => {
-        if(err) return res.json({Status: false, Error: "Query error"})
-        return res.json({Status: true})
+        if (err) return res.json({ Status: false, Error: "Query error" })
+        return res.json({ Status: true })
+    });
+});
+
+// Image upload
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'Public/Images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({
+    storage: storage
+})
+
+// End of image ulpoad
+
+router.post('/add_employee', upload.single('image'), (req, res) => {
+    const sql = `INSERT INTO employee
+    (name, email, password, address, salary, image, category_id)
+    VALUES (?)`;
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) return res.json({ Status: false, Error: "Query error" })
+        const values = [
+            req.body.name,
+            req.body.email,
+            hash,
+            req.body.address,
+            req.body.salary,
+            req.file.filename,
+            req.body.category_id
+        ]
+        con.query(sql, [values], (err, result) => {
+            if (err) return res.json({ Status: false, Error: "Query error" })
+            return res.json({ Status: true })
+        })
+    })
+});
+
+router.get('/employee', (req, res) => {
+    const sql = "SELECT * FROM employee";
+    con.query(sql, (err, result) => {
+        if (err) return res.json({ Status: false, Error: "Query error" })
+        return res.json({ Status: true, Result: result })
     });
 });
 
--- Server/index.js
+++ Server/index.js
@@ -11,6 +11,7 @@ app.use(cors({
 app.use(express.json());
 
 app.use('/auth', AdminRouter);
+app.use(express.static('Public'));
 
 app.listen(3001, () => {
     console.log("Server is running");
